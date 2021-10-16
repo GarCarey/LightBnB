d1f5d1c0ce4401e53664f912ce47d19b987f2327
Gareth <gar.carey@gmail.com>
2021-10-16 18:17:40 -0400

    add listerners to reserve button

--- LightBnB_WebApp-master/public/javascript/index.js
+++ LightBnB_WebApp-master/public/javascript/index.js
@@ -2,5 +2,9 @@ $(() => {
   getAllListings().then(function( json ) {
     propertyListings.addProperties(json.properties);
     views_manager.show('listings');
+    $(document).on('click','.reserve-button', function() {
+      const idData = $(this).attr('id').substring(17);
+      views_manager.show('newReservation', idData);
+    })
   });
 });
